9a6f84b5477400c7d2a0c6d7b68a331fce1294d7
willmac1 <maclachlanwill@gmail.com>
2016-07-07 20:42:17 +0100

    Revert "v0.0.1"
    This reverts commit 1298937c8ed5a884a900f4b6c01ed53fdd1900ee.

--- main.lua
+++ main.lua
@@ -5,14 +5,21 @@ local HC = require "HC" --collision detection
 math.randomseed(os.time())
 
 function gen_monster(class)
-    local monster_height=img:getWidth()
-    local monster_width =img:getHeight()
+    local monster_height=images[class].normal[1]:getWidth()
+    local monster_width =images[class].normal[1]:getWidth()
 
-    monster={state={class,"normal",1},rect=HC.rectangle(width+monster_width*2,math.random(0,height),monster_width,monster_height),dx=math.random(-1,-5),dy=0}
+    monster={state={class,"normal"},rect=HC.rectangle(width+pawn_width*2,math.random(0,height),pawn_width,pawn_height),dx=math.random(-1,-5),dy=0}
         
     table.insert(monsters,monster)
 end
 
+function string.starts(String,Start)
+   return string.sub(String,1,string.len(Start))==Start
+end
+
+function string.ends(String,End)
+   return End=='' or string.sub(String,-string.len(End))==End
+end
 
 function merge(...)
     path=""
@@ -24,6 +31,30 @@ function merge(...)
     return path
 end
 
+function get_states(dir,name)
+    local states={}
+
+    if name==nil then name="" end
+
+    for x,obj in ipairs(love.filesystem.getDirectoryItems(merge(dir,name))) do
+
+        print(obj)
+        if string.ends(obj,".png") then
+            table.insert(states,love.graphics.newImage(merge(dir,name,obj)))
+        else
+            states[obj]=get_states(merge(dir,name),obj) states[name]=nil
+
+            if string.starts(obj,"loop_") then 
+                table.insert(states[obj],0)
+                states[name:gsub("%loop_","")]=states[obj]
+                states[obj]=nil
+            end
+        end
+    end
+
+    return states
+end
+
 function love.load()
     width=800
     height=600
@@ -31,8 +62,10 @@ function love.load()
     love.window.setMode(width,height)
     love.graphics.setBackgroundColor(255,255,255)
     
-    img=love.graphics.newImage("Images/zombie.png")
+    images=get_states("Images")
 
+    print(inspect(images))
+    
     monsters={}
 end
     
@@ -49,6 +82,6 @@ function love.draw()
     
     for x,monster in ipairs(monsters) do
         local mx,my = monster.rect:bbox()
-        love.graphics.draw(img,mx,my)
+        love.graphics.draw(images[monster.image],mx,my)
     end
 end
